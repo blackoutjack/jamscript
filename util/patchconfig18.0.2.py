faf1c2d99dbe96dd391eba3a2cbe68a7ff5c724f
Rich Joiner <joiner@cs.wisc.edu>
2015-01-19 17:30:46 -0600

    Ported JAMScript to Firefox 18.0.2

---
+++ util/patchconfig18.0.2.py
@@ -0,0 +1,56 @@
+
+ORIGDIR = "ff-orig/mozilla-18.0.2"
+DEVDIR = "ff-dev/mozilla-18.0.2"
+PATCHDIR = "patch/mozilla-18.0.2"
+IGNORE_BINARY = True
+EXCLUSIONS = [
+  '_virtualenv',
+  'confdefs.h',
+  '*.pytmp',
+  'obj-*',
+  'dist',
+  '*.pyc',
+  '*.jar',
+  '.hg*',
+  '*.so',
+  'configure',
+  'configure.in',
+  '*.a',
+  'Linux_All_DBG.OBJ',
+  '*.o',
+  '*.swp',
+  '*.swo',
+  'ipc',
+  'TestDeadlockDetector*',
+  'test_deadlock_detector',
+  'idl-parser',
+  '.mozconfig*',
+  '.project',
+  'jprof-log',
+  'imacros.c.out*',
+  'build-debug',
+  'PKG-INFO',
+  'mozbase',
+  'python',
+  'mozinfo.json',
+  '*.mk',
+  # Below here, we exclude to allow comparison of a compiled js/src
+  # directory to an uncompiled one.
+  '.deps',
+  'system_wrappers_js',
+  'Makefile',
+  'config.cache',
+  'config.log',
+  'config.status',
+  'host_*',
+  'js-conf*',
+  '*.a',
+  '*.a.desc',
+  'nsinstall',
+  'expandlibs_config.py',
+  'jsapi-tests',
+  'jsauto*',
+  '*.out.h',
+  'unallmakefiles',
+]
+
